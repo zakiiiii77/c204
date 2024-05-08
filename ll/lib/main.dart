78ac9bbd2898a73f10b350d2e09e5dfeb09b9f93
zakiiiii77 <125500686+zakiiiii77@users.noreply.github.com>
2024-05-09 00:29:03 +0300

    Update main.dart

--- ll/lib/main.dart
+++ ll/lib/main.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:ll/api/api.dart';
 import 'package:ll/pages/auth_page.dart';
 import 'firebase_options.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  await FirebaseApi().iniNotifications();
   runApp(const MyApp());
 }
 
